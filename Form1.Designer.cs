80c8b98bd83b170eb204ba0f2abcf3c9a924f795
Renier <renier.smith@webafrica.org.za>
2025-03-05 14:09:09 +0200

    Autoclicker works. Needs refinement and more features.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,18 +29,29 @@
         private void InitializeComponent()
         {
             menuStrip1 = new MenuStrip();
+            informationToolStripMenuItem = new ToolStripMenuItem();
             eXITToolStripMenuItem = new ToolStripMenuItem();
+            textboxEnableClick = new CheckBox();
+            labelOutput1 = new Label();
+            buttonStart = new Button();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip1
             // 
-            menuStrip1.Items.AddRange(new ToolStripItem[] { eXITToolStripMenuItem });
+            menuStrip1.Items.AddRange(new ToolStripItem[] { informationToolStripMenuItem, eXITToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Size = new Size(800, 24);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
+            // 
+            // informationToolStripMenuItem
+            // 
+            informationToolStripMenuItem.Name = "informationToolStripMenuItem";
+            informationToolStripMenuItem.Size = new Size(82, 20);
+            informationToolStripMenuItem.Text = "Information";
+            informationToolStripMenuItem.Click += informationToolStripMenuItem_Click;
             // 
             // eXITToolStripMenuItem
             // 
@@ -49,14 +60,48 @@
             eXITToolStripMenuItem.Text = "Exit";
             eXITToolStripMenuItem.Click += eXITToolStripMenuItem_Click;
             // 
+            // textboxEnableClick
+            // 
+            textboxEnableClick.AutoSize = true;
+            textboxEnableClick.Location = new Point(12, 81);
+            textboxEnableClick.Name = "textboxEnableClick";
+            textboxEnableClick.Size = new Size(57, 19);
+            textboxEnableClick.TabIndex = 1;
+            textboxEnableClick.Text = "Click?";
+            textboxEnableClick.UseVisualStyleBackColor = true;
+            textboxEnableClick.CheckedChanged += textboxEnableClick_CheckedChanged;
+            // 
+            // labelOutput1
+            // 
+            labelOutput1.AutoSize = true;
+            labelOutput1.Location = new Point(125, 82);
+            labelOutput1.Name = "labelOutput1";
+            labelOutput1.Size = new Size(51, 15);
+            labelOutput1.TabIndex = 2;
+            labelOutput1.Text = "Output1";
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(12, 121);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(106, 63);
+            buttonStart.TabIndex = 3;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
             // AutoClicker
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(buttonStart);
+            Controls.Add(labelOutput1);
+            Controls.Add(textboxEnableClick);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "AutoClicker";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Auto Clicker";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
@@ -68,5 +113,9 @@
 
         private MenuStrip menuStrip1;
         private ToolStripMenuItem eXITToolStripMenuItem;
+        private CheckBox textboxEnableClick;
+        private Label labelOutput1;
+        private Button buttonStart;
+        private ToolStripMenuItem informationToolStripMenuItem;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,24 +1,92 @@
 using System;
 using System.Runtime.InteropServices;
-
-//Imports
-[DllImport("user32.dll")]
-static extern void mouse_event(unit dwFlags, unit dx, )
-
+using System.Threading;
 
 namespace AutoClicker
 
 {
     public partial class AutoClicker : Form
     {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
+
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const uint MOUSEEVENTF_LEFTUP = 0x04;
+
+        private bool isClickHold = false;
+        int milisecondsWait = 500;
+
+        int count = 0;
+
         public AutoClicker()
         {
             InitializeComponent();
+            this.MouseClick += mouseClick;
+
+            this.KeyPreview = true;
+            this.KeyDown += Cancel_KeyDown;
         }
 
         private void eXITToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void mouseClick(object sender, MouseEventArgs e)
+        {
+
+             isClickHold = true;
+             if (Control.ModifierKeys.ToString() == "Alt")
+             {
+                DoMouseClick();
+                buttonStart.Text = "Clicking...";
+             }
+        }
+
+        private void DoMouseClick()
+        {
+            new Thread(() =>
+            {
+                while (isClickHold)
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    uint X = (uint)Cursor.Position.X;
+                    uint Y = (uint)Cursor.Position.Y;
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, UIntPtr.Zero);
+                    mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, UIntPtr.Zero);
+                    Thread.Sleep(milisecondsWait);
+                }
+            }).Start();
+        }
+
+        public void labelIncrement()
+        {
+            count++;
+            labelOutput1.Text = count.ToString();
+        }
+
+        private void textboxEnableClick_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cancel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.C && e.Shift)
+            {
+                isClickHold = false;
+                buttonStart.Text = "Idle";
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            labelIncrement();
+        }
+
+        private void informationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Information");
         }
     }
 }
