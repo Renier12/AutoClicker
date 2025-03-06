58a0bbca6f8efde01153c16408b137d291aff061
Renier <renier.smith@webafrica.org.za>
2025-03-06 12:47:42 +0200

    Version 1 All key features added and working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoClicker));
             menuStrip1 = new MenuStrip();
             informationToolStripMenuItem = new ToolStripMenuItem();
             eXITToolStripMenuItem = new ToolStripMenuItem();
-            textboxEnableClick = new CheckBox();
             labelOutput1 = new Label();
             buttonStart = new Button();
+            textboxEnableClick = new CheckBox();
+            labelDelay = new Label();
+            textBoxDelay = new TextBox();
+            textBoxRandMin = new TextBox();
+            textBoxRandMax = new TextBox();
+            labelRandMin = new Label();
+            labelRandMax = new Label();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -42,7 +49,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { informationToolStripMenuItem, eXITToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.Size = new Size(224, 24);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -61,49 +68,113 @@
             eXITToolStripMenuItem.Text = "Exit";
             eXITToolStripMenuItem.Click += eXITToolStripMenuItem_Click;
             // 
-            // textboxEnableClick
-            // 
-            textboxEnableClick.AutoSize = true;
-            textboxEnableClick.Location = new Point(12, 81);
-            textboxEnableClick.Name = "textboxEnableClick";
-            textboxEnableClick.Size = new Size(57, 19);
-            textboxEnableClick.TabIndex = 1;
-            textboxEnableClick.Text = "Click?";
-            textboxEnableClick.UseVisualStyleBackColor = true;
-            textboxEnableClick.CheckedChanged += textboxEnableClick_CheckedChanged;
-            // 
             // labelOutput1
             // 
             labelOutput1.AutoSize = true;
-            labelOutput1.Location = new Point(125, 82);
+            labelOutput1.Location = new Point(81, 177);
             labelOutput1.Name = "labelOutput1";
             labelOutput1.Size = new Size(51, 15);
             labelOutput1.TabIndex = 2;
             labelOutput1.Text = "Output1";
+            labelOutput1.Click += labelOutput1_Click;
             // 
             // buttonStart
             // 
-            buttonStart.Location = new Point(12, 121);
+            buttonStart.Location = new Point(12, 111);
             buttonStart.Name = "buttonStart";
-            buttonStart.Size = new Size(106, 63);
+            buttonStart.Size = new Size(198, 63);
             buttonStart.TabIndex = 3;
-            buttonStart.Text = "Start";
+            buttonStart.Text = "Status";
             buttonStart.UseVisualStyleBackColor = true;
             buttonStart.Click += buttonStart_Click;
+            // 
+            // textboxEnableClick
+            // 
+            textboxEnableClick.AutoSize = true;
+            textboxEnableClick.Location = new Point(12, 63);
+            textboxEnableClick.Name = "textboxEnableClick";
+            textboxEnableClick.Size = new Size(154, 19);
+            textboxEnableClick.TabIndex = 1;
+            textboxEnableClick.Text = "Use Random Times (ms)";
+            textboxEnableClick.UseVisualStyleBackColor = true;
+            textboxEnableClick.CheckedChanged += textboxEnableClick_CheckedChanged;
+            // 
+            // labelDelay
+            // 
+            labelDelay.AutoSize = true;
+            labelDelay.Location = new Point(12, 37);
+            labelDelay.Name = "labelDelay";
+            labelDelay.Size = new Size(63, 15);
+            labelDelay.TabIndex = 4;
+            labelDelay.Text = "Delay (ms)";
+            // 
+            // textBoxDelay
+            // 
+            textBoxDelay.Location = new Point(81, 34);
+            textBoxDelay.Name = "textBoxDelay";
+            textBoxDelay.Size = new Size(100, 23);
+            textBoxDelay.TabIndex = 5;
+            textBoxDelay.Text = "100";
+            // 
+            // textBoxRandMin
+            // 
+            textBoxRandMin.Enabled = false;
+            textBoxRandMin.Location = new Point(46, 82);
+            textBoxRandMin.Name = "textBoxRandMin";
+            textBoxRandMin.Size = new Size(61, 23);
+            textBoxRandMin.TabIndex = 7;
+            textBoxRandMin.TextChanged += textBox2_TextChanged;
+            // 
+            // textBoxRandMax
+            // 
+            textBoxRandMax.Enabled = false;
+            textBoxRandMax.Location = new Point(149, 82);
+            textBoxRandMax.Name = "textBoxRandMax";
+            textBoxRandMax.Size = new Size(61, 23);
+            textBoxRandMax.TabIndex = 8;
+            textBoxRandMax.TextChanged += textBoxRandMax_TextChanged;
+            // 
+            // labelRandMin
+            // 
+            labelRandMin.AutoSize = true;
+            labelRandMin.Enabled = false;
+            labelRandMin.Location = new Point(12, 85);
+            labelRandMin.Name = "labelRandMin";
+            labelRandMin.Size = new Size(28, 15);
+            labelRandMin.TabIndex = 9;
+            labelRandMin.Text = "Min";
+            labelRandMin.Click += labelRandMin_Click;
+            // 
+            // labelRandMax
+            // 
+            labelRandMax.AutoSize = true;
+            labelRandMax.Enabled = false;
+            labelRandMax.Location = new Point(113, 85);
+            labelRandMax.Name = "labelRandMax";
+            labelRandMax.Size = new Size(30, 15);
+            labelRandMax.TabIndex = 10;
+            labelRandMax.Text = "Max";
             // 
             // AutoClicker
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(224, 202);
+            Controls.Add(labelRandMax);
+            Controls.Add(labelRandMin);
+            Controls.Add(textBoxRandMax);
+            Controls.Add(textBoxRandMin);
+            Controls.Add(textBoxDelay);
+            Controls.Add(labelDelay);
             Controls.Add(buttonStart);
             Controls.Add(labelOutput1);
             Controls.Add(textboxEnableClick);
             Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = menuStrip1;
             Name = "AutoClicker";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Auto Clicker";
+            Text = "AutoClicker";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             ResumeLayout(false);
@@ -114,9 +185,15 @@
 
         private MenuStrip menuStrip1;
         private ToolStripMenuItem eXITToolStripMenuItem;
-        private CheckBox textboxEnableClick;
         private Label labelOutput1;
         private Button buttonStart;
         private ToolStripMenuItem informationToolStripMenuItem;
+        private CheckBox textboxEnableClick;
+        private Label labelDelay;
+        private TextBox textBoxDelay;
+        private TextBox textBoxRandMin;
+        private TextBox textBoxRandMax;
+        private Label labelRandMin;
+        private Label labelRandMax;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,6 @@ namespace AutoClicker
     {
         [DllImport("user32.dll", SetLastError = true)]
         private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
-
         private const uint MOUSEEVENTF_LEFTDOWN = 0x02;
         private const uint MOUSEEVENTF_LEFTUP = 0x04;
 
@@ -23,14 +22,16 @@ namespace AutoClicker
         private const int VK_F2 = 0x71; // F2 key
 
         private bool isClickHold = false;
-        int milisecondsWait = 500;
+        int milisecondsWait = 100;
+        int randMin = 20;
+        int randMax = 100;
 
         int count = 0;
 
         public AutoClicker()
         {
             InitializeComponent();
-
+            labelOutput1.Text = string.Empty;
             RegisterHotKey(this.Handle, 1, 0, VK_F1); // Register F1
             RegisterHotKey(this.Handle, 2, 0, VK_F2); // Register F2
         }
@@ -42,8 +43,12 @@ namespace AutoClicker
                 if (id == 1)
                 {
                     isClickHold = true;
+                    setMilisecondDelay(); //this is done afer isClickHold is set to ensure delay is valid
+                    if (textboxEnableClick.Checked)
+                    {
+                        setRandValues();
+                    }
                     DoMouseClick();
-                    buttonStart.Text = "Clicking...";
                 }
                 else if (id == 2)
                 {
@@ -59,31 +64,98 @@ namespace AutoClicker
             this.Close();
         }
 
+        private void setMilisecondDelay()
+        {
+            if (!int.TryParse(textBoxDelay.Text, out milisecondsWait))
+            {
+                MessageBox.Show("Delay must be an integer");
+                textBoxDelay.Text = "100";
+                isClickHold = false;
+            }
+        }
+
+        private void setRandValues() //Check if randValues are valid
+        {
+            if (!int.TryParse(textBoxRandMin.Text, out randMin))
+            {
+                MessageBox.Show("Minimum value must be an interger!");
+                isClickHold = false;
+                textBoxRandMin.Text = "20";
+            }
+
+            if (!int.TryParse(textBoxRandMax.Text, out randMax))
+            {
+                MessageBox.Show("Maximum value must be an interger!");
+                isClickHold = false;
+                textBoxRandMax.Text = milisecondsWait.ToString();
+            }
+        }
+
         private void DoMouseClick()
         {
-            new Thread(() =>
+            buttonStart.Text = "Clicking...";
+            if (textboxEnableClick.Checked)
             {
-                while (isClickHold)
+                new Thread(() =>
                 {
-                    Thread.CurrentThread.IsBackground = true;
-                    uint X = (uint)Cursor.Position.X;
-                    uint Y = (uint)Cursor.Position.Y;
-                    mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, UIntPtr.Zero);
-                    mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, UIntPtr.Zero);
-                    Thread.Sleep(milisecondsWait);
-                }
-            }).Start();
+                    Random rand = new Random();
+                    while (isClickHold)
+                    {
+                        milisecondsWait = rand.Next(randMin, randMax);
+                        Thread.CurrentThread.IsBackground = true;
+                        uint X = (uint)Cursor.Position.X;
+                        uint Y = (uint)Cursor.Position.Y;
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, UIntPtr.Zero);
+                        mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, UIntPtr.Zero);
+                        Thread.Sleep(milisecondsWait);
+                    }
+                }).Start();
+            }
+            else
+            {
+                new Thread(() =>
+                {
+                    while (isClickHold)
+                    {
+                        Thread.CurrentThread.IsBackground = true;
+                        uint X = (uint)Cursor.Position.X;
+                        uint Y = (uint)Cursor.Position.Y;
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, UIntPtr.Zero);
+                        mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, UIntPtr.Zero);
+                        Thread.Sleep(milisecondsWait);
+                    }
+                }).Start();
+            }
         }
 
         public void labelIncrement()
         {
             count++;
-            labelOutput1.Text = count.ToString();
+            labelOutput1.Text = "Clicks: " + count.ToString();
         }
 
         private void textboxEnableClick_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (textboxEnableClick.Checked)
+            {
+                labelRandMax.Enabled = true;
+                textBoxRandMax.Enabled = true;
+                textBoxRandMax.Text = "100";
+                labelRandMin.Enabled = true;
+                textBoxRandMin.Enabled = true;
+                textBoxRandMin.Text = "20";
+                labelDelay.Enabled = false;
+                textBoxDelay.Enabled = false;
+            }
+            else
+            {
+                labelRandMax.Enabled = false;
+                textBoxRandMax.Enabled = false;
+                labelRandMin.Enabled = false;
+                textBoxRandMin.Enabled = false;
+                labelDelay.Enabled = true;
+                textBoxDelay.Enabled = true;
+            }
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
@@ -92,13 +164,34 @@ namespace AutoClicker
         }
 
         private void informationToolStripMenuItem_Click(object sender, EventArgs e)
-        {           
-            MessageBox.Show("Information");            
+        {
+            MessageBox.Show("Press 'F1' to start" + '\n' +
+                "Press 'F2' to stop");
         }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             UnregisterHotKey(this.Handle, 1);
             UnregisterHotKey(this.Handle, 2);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxRandMax_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelRandMin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelOutput1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
